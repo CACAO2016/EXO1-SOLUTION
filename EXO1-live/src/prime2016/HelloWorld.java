8b5109c2361b690efbaa600bdd71255429f3128e
Massimo Tisi <massimo.tisi@mines-nantes.fr>
2016-03-11 15:09:20 +0100

    C

--- EXO1-live/src/prime2016/HelloWorld.java
+++ EXO1-live/src/prime2016/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello A!");
-		System.out.println("Hello B!");
+		System.out.println("Hello B and C!");
 	}
 
 }
